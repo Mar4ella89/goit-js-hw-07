41e82f47f570a62a8259ee42dbe3ff1ca0326653
Marat Baikin <3371335@gmail.com>
2022-11-13 23:10:31 +0200

    Complited task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const ref = {
+  imgRef: document.querySelector("ul.gallery"),
+};
+
+const creatImgList = galleryItems
+  .map(
+    (item) =>
+      `<li><a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" data-source="${item.original}" />
+</a></li>`
+  )
+  .join("");
+
+const imgAdd = () => {
+  ref.imgRef.insertAdjacentHTML("beforeend", creatImgList);
+};
+
+imgAdd();
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  disableScroll: false,
+  scrollZoom: false,
+});
+
